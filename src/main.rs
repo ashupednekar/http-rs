3a129f73f6e1f11309105a7e2b7f06ed722a36ca
Ashu Pednekar <ashupednekar49@gmail.com>
2025-02-07 06:47:41 +0530

    feat: http echo

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use cmd::run;
 
 mod cmd;
 pub mod pkg;
+pub mod conf;
 pub mod prelude;
 
 use prelude::Result;
 
 #[tokio::main]
 async fn main() -> Result<()>{
+    tracing_subscriber::fmt::init();
     run().await?;
     Ok(())
 }
--- src/pkg/server/listen.rs
+++ src/pkg/server/listen.rs
@@ -1,8 +1,36 @@
-use crate::prelude::Result;
-
-
+use crate::{conf::settings, prelude::Result};
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpListener};
 
 pub async fn listen() -> Result<()>{
-
-    Ok(())
+    let addr = format!("0.0.0.0:{}", &settings.listen_port);
+    let ln = TcpListener::bind(&addr).await?;
+    tracing::info!("listening at {}", &addr);
+    loop {
+        let (mut socket, _) = ln.accept().await?;
+        tokio::spawn(async move{
+            let mut buf = vec![0; 1024];
+            loop {
+                let n = socket
+                    .read(&mut buf)
+                    .await
+                    .expect("failed to read from socket"); 
+                if n == 0{
+                    return;
+                }
+                if let Ok(msg) = String::from_utf8(buf[..n].to_vec()) {
+                    tracing::info!("{}", msg);
+                    let res = format!(
+                        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: text/plain\r\n\r\n{}",
+                        msg.len(),
+                        msg
+                    );
+                    if let Err(e) = socket.write_all(res.as_bytes()).await{
+                        tracing::error!("error writing to stream: {}", &e);
+                    }
+                } else {
+                    tracing::warn!("received non-UTF8 data");
+                }
+            }
+        });
+    }
 }
